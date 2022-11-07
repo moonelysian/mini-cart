993dddae3d73af816856ba59a2a4ccec06e65002
moonelysian <dandyblack@naver.com>
2022-11-07 14:49:55 +0900

    feat($요구사항1): 비동기 API 요청 에러핸들링 추가

--- src/api/ProductApi.js
+++ src/api/ProductApi.js
@@ -1,6 +1,19 @@
+const request = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      const error = await response.json();
+      throw error; 
+    }
+    const data = await response.json();
+    return data
+  } catch(e) {
+    console.log(e);
+  }
+} 
+
 const getProducts = async () => {
-  const response = await fetch('./api/productData.json');
-  const products = await response.json();
+  const products = await request('./api/productData.json');
   return products;
 }
 
